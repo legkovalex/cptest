42ccc2c541e46717758f3b15e72fdd75ca712fab
Legkov Alexey <legkovalex@gmail.com>
2019-10-05 17:53:45 +0700

    work began

--- docker/runner.bash
+++ docker/runner.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 /etc/init.d/nginx start
 /etc/init.d/haproxy start
+sleep 1000
+# haproxy -f /etc/haproxy/haproxy.cfg
--- init.bash
+++ init.bash
@@ -1,11 +1,4 @@
 init () {
-    apt-get update
-    apt-get install -y git \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg-agent \
-        software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     apt-key fingerprint 0EBFCD88
     add-apt-repository \
@@ -13,8 +6,43 @@ init () {
         $(lsb_release -cs) \
         stable"
     apt-get update
-    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
+    apt-get install -y git \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io \
+        docker-compose
 }
 
+build() {
+    docker build -t my_web docker/
+}
+
+start() {
+    docker-compose up -d
+}
+
+check() {
+wget -q http://127.0.0.1:80
+if [ $? -eq 0 ]; then
+    echo "Online"
+else
+    echo "Offline"
+fi
+
+wget -q http://127.0.0.1:81
+if [ $? -eq 0 ]; then
+    echo "Online"
+else
+    echo "Offline"
+fi
+}
 
 init
+build
+start
+check
